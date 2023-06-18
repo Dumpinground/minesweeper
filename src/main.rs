3415bee4c7074df315633e86e5d9feda9521792f
Dumpinground <e60533658@icloud.com>
2023-06-18 23:41:25 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use bevy::{prelude::*, window::WindowResolution};
+
+#[cfg(feature = "debug")]
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            title: "Mine Sweeper!".into(),
+            resolution: WindowResolution::new(700., 800.),
+            ..default()
+        }),
+        ..default()
+    }));
+
+    #[cfg(feature = "debug")]
+    app.add_plugin(WorldInspectorPlugin::new());
+
+    app.add_startup_system(camera_setup);
+
+    app.run();
+}
+
+fn camera_setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
